3357efd730fd17b6a18c09f5691754a15cd2f8be
Shuo Chen <shuo.chen@epigenesys.org.uk>
2022-05-30 14:38:56 +0100

    fixing background img issue for ios

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -3,3 +3,12 @@
 // import "whatwg-fetch";
 
 import './RSVPForm';
+
+const resizeWindow = () => {
+  if (('ontouchstart' in window) && ((navigator.userAgent.match(/iPad|iPhone|iPod|Macintosh/i) != null) || (navigator.platform.match(/i(Phone|Pod)/i) != null))) {
+    document.body.style.minHeight = `${window.innerHeight}px`;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', resizeWindow);
+window.addEventListener('resize', resizeWindow);
